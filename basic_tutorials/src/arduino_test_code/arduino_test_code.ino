7fcc1df0e939f0afa500f88dc3b120b716eaece6
HwiwonSeo <cv5006@naver.com>
2020-06-29 23:22:26 +0900

    Update arduino test code

--- basic_tutorials/src/arduino_test_code/arduino_test_code.ino
+++ basic_tutorials/src/arduino_test_code/arduino_test_code.ino
@@ -1,9 +1,22 @@
+#define WRITE_PERIOD 500 //ms
+
+int recieved_data = 0;
+unsigned int ms_timer = 0;
+
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if(Serial.available()){
+    recieved_data = Serial.parseInt();
+  }
+
+  if(ms_timer > WRITE_PERIOD){
+    Serial.println(recieved_data);
+    ms_timer = 0;
+  }
   
+  ms_timer++;
+  delay(1);
 }
