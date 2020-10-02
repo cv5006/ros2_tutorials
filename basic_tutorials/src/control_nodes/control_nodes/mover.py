aef3e42143da2403a9a26f0c2ba23efaf967e7e6
HwiwonSeo <cv5006@naver.com>
2020-10-03 01:19:39 +0900

    1002 bak

--- basic_tutorials/src/control_nodes/control_nodes/mover.py
+++ basic_tutorials/src/control_nodes/control_nodes/mover.py
@@ -17,10 +17,11 @@ class MoveServer(Node):
             'move',
             self.exe_callback
         )
-
+        self.pub = self.create_publisher(Int16, 'motor', 10)
         self.cli = self.create_client(SetBool, 'sine_wave/run')
 
-        self.pos = 0
+        self.pos = Int16()
+        self.pos.data = 0
         self.tol = 1
         
 
@@ -34,23 +35,25 @@ class MoveServer(Node):
         result_msg =  Move.Result()
         goal = hgoal.request.destination
         
-        self.get_logger().info('Move from: %d → to: %d' % (self.pos, goal))
+        self.get_logger().info('Move from: %d → to: %d' % (self.pos.data, goal))
         
+        lim = 20
         i = 0
         while True:
-            err = self.pos - goal
+            err = (self.pos.data - goal)
             
-            self.pos = self.pos - int((err>0) - (err<0))
-            feedback_msg.footprint = self.pos
+            self.pos.data = self.pos.data - (err if abs(err) < lim else int((err>0)-(err<0))*lim)
+            self.pub.publish(self.pos)
+            feedback_msg.footprint = self.pos.data
             hgoal.publish_feedback(feedback_msg)
-            sleep(0.01)
+            sleep(0.1)
             
             if abs(err) <= self.tol : hgoal.succeed(); break
-            if i > 100 :              hgoal.abort(); break
+            if i > 30 :              hgoal.abort(); break
             
             i = i + 1
         
-        result_msg.position = self.pos
+        result_msg.position = self.pos.data
 
         return result_msg
 
