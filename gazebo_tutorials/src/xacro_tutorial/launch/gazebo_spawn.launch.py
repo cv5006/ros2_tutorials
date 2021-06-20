afbda06b9316147af3117300608dc135e3b8e0cd
seo <cv5006@naver.com>
2021-06-20 20:26:35 +0900

    commit current state

--- gazebo_tutorials/src/xacro_tutorial/launch/gazebo_spawn.launch.py
+++ gazebo_tutorials/src/xacro_tutorial/launch/gazebo_spawn.launch.py
@@ -4,7 +4,8 @@ from ament_index_python.packages import get_package_share_directory
 
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, ExecuteProcess, RegisterEventHandler
+from launch.event_handlers import OnProcessExit
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 from launch_ros.actions import Node
@@ -65,10 +66,32 @@ def generate_launch_description():
         output='screen'
     )
 
+    load_joint_state_controller = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_start_controller', 'joint_state_controller'],
+        output='screen'
+    )
+
+    load_effort_controller = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_start_controller', 'effort_controllers'],
+        output='screen'
+    )
+
     return LaunchDescription([
-        controller_manager,
+        # RegisterEventHandler(
+        #     event_handler=OnProcessExit(
+        #         target_action=spawn_entity,
+        #         on_exit=[load_joint_state_controller],
+        #     )
+        # ),
+        # RegisterEventHandler(
+        #     event_handler=OnProcessExit(
+        #         target_action=load_joint_state_controller,
+        #         on_exit=[load_effort_controller],
+        #     )
+        # ),
+
+        # controller_manager,
         gazebo,
         node_robot_state_publisher,
         spawn_entity,
-        
     ])
---
+++ scripts/gzweb_installation.sh
@@ -0,0 +1,22 @@
+# http://gazebosim.org/gzweb.html#install-collapse-0
+
+sudo apt install libjansson-dev libboost-dev imagemagick libtinyxml-dev mercurial cmake build-essential -y
+
+ # install nvm
+ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+
+ # source .bashrc so we can use the nvm cmd
+ source ~/.bashrc
+
+ # install node. Supported versions are 8 to 11. 
+ nvm install 8
+
+
+#  git clone https://github.com/osrf/gzweb
+#  git checkout gzweb_1.4.1
+#  source /usr/share/gazebo/setup.sh
+#  npm run deploy --- -m -t
+
+# gzweb color issue
+# https://support.yonohub.com/t/gzweb-media-directory-is-missing/144
+# copy /usr/share/gazebo-9/media to ~/gzweb/http/client/assets
